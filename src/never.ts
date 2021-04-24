df013a61bc76f4a23bfbb9ce5a31dae72e822f71
Matt Hinz <matthinz@gmail.com>
2021-04-24 09:51:27 -0700

    Fix lint issues

--- src/never.ts
+++
@@ -1,8 +0,0 @@
-import { ValidationContext, Validator } from "./types";
-
-export class NeverValidator<Type> implements Validator<Type> {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  validate(input: any, _context?: ValidationContext): input is Type {
-    return false;
-  }
-}
